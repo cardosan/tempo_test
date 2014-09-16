a66f4fb7910d734e8be5b883fb12f18d044bd471
Chris Mutel <cmutel@gmail.com>
2014-09-16 17:54:55 +0200

    Better logging

--- bw2temporalis/dynamic_lca.py
+++ bw2temporalis/dynamic_lca.py
@@ -11,6 +11,15 @@ import pprint
 import warnings
 
 
+class FakeLog(object):
+    """Like a log object, but does nothing"""
+    def fake_function(cls, *args, **kwargs):
+        return
+
+    def __getattr__(self, attr):
+        return self.fake_function
+
+
 class DynamicLCA(object):
     """Calculate a dynamic LCA, where processes, emissions, and CFs can vary throughout time."""
     def __init__(self, demand, dynamic_method, worst_case_method, now=None, max_calc_number=1e4, cutoff = 0.001, log=False):
@@ -20,7 +29,7 @@ class DynamicLCA(object):
         self.now = now or arrow.now()
         self.max_calc_number = max_calc_number
         self.cutoff_value = cutoff
-        self.log = get_logger("dynamic-lca.log") if log else None
+        self.log = get_logger("dynamic-lca.log") if log else FakeLog()
 
     def calculate(self):
         self.timeline = Timeline()
@@ -31,16 +40,24 @@ class DynamicLCA(object):
         self.gt_results = self.gt.calculate(self.demand, self.worst_case_method)
         self.lca = self.gt_results['lca']
         self.temporal_edges = self.get_temporal_edges()
-        self.cutoff = abs(self.lca.score) * self.cutoff_value
+        self.cutoff = abs(self.lca.score * self.cutoff_value)
         self.gt_nodes = GTManipulator.add_metadata(
             self.gt_results['nodes'],
             self.lca
         )
         self.gt_edges = self.translate_edges(self.gt_results['edges'])
 
-        if self.log:
-            self.log.info("NODES: " + pprint.pformat(self.gt_nodes))
-            self.log.info("EDGES: " + pprint.pformat(self.gt_edges))
+        self.log.info(u"Starting dynamic LCA")
+        self.log.info(u"Demand: %s" % self.demand)
+        self.log.info(u"Dynamic method: %s" % unicode(self.dynamic_method))
+        self.log.info(u"Worst case method: %s" % unicode(self.worst_case_method))
+        self.log.info(u"Start datetime: %s" % self.now.isoformat())
+        self.log.info(u"Maximum calculations: %i" % self.max_calc_number)
+        self.log.info(u"Worst case LCA score: %.4g." % self.lca.score)
+        self.log.info(u"Cutoff value (fraction): %.4g." % self.cutoff_value)
+        self.log.info(u"Cutoff score: %.4g." % self.cutoff)
+        self.log.debug("NODES: " + pprint.pformat(self.gt_nodes))
+        self.log.debug("EDGES: " + pprint.pformat(self.gt_edges))
 
         # Initialize heap
         heappush(
@@ -106,7 +123,12 @@ class DynamicLCA(object):
 
     def discard_node(self, node, amount):
         self.lca.redo_lcia({node: amount})
-        return abs(self.lca.score) < self.cutoff
+        discard = abs(self.lca.score) < self.cutoff
+        if discard:
+            self.log.info(u"Discarding node: %s of %s (score %.4g)" % (
+                          amount, node, self.lca.score)
+            )
+        return discard
 
     def to_timedelta(self, years):
         return datetime.timedelta(hours=int(years * 8765.81))
@@ -116,8 +138,7 @@ class DynamicLCA(object):
             return dt
         ds_data = Database(ds[0]).load()[ds]
         absolute = "absolute date" in ds_data
-        if self.log:
-            self.log.info("check_absolute: %s (%s)" % (absolute, ds))
+        self.log.info("check_absolute: %s (%s)" % (absolute, ds))
         if "absolute date" in ds_data:
             return arrow.get(ds_data['absolute date'])
         else:
@@ -128,12 +149,13 @@ class DynamicLCA(object):
 
         dt = self.check_absolute(ds, dt)
 
-        if self.log:
-            self.log.info(".iterate(): %s, %s, %s" % (ds, dt, amount))
+        self.log.info(".iterate(): %s, %s, %s" % (ds, dt, amount))
         self.add_biosphere_flows(ds, dt, amount)
         for edge in self.tech_edges_from_node(ds):
-            if self.log:
-                self.log.info(".iterate:edge: " + pprint.pformat(edge))
+            self.log.info(
+                ("Iterate edge: (calc. number %s)\n" % self.calc_number) + \
+                pprint.pformat(edge)
+            )
             input_amount = amount * edge['exc_amount']
             if self.discard_node(edge['from'], input_amount):
                 continue
@@ -144,7 +166,7 @@ class DynamicLCA(object):
 
             for year_delta, subtotal in temporal_edges:
                 heappush(self.heap, (
-                    self.lca.score * subtotal / amount,
+                    abs(self.lca.score * subtotal / amount),
                     edge['from'],
                     dt + self.to_timedelta(year_delta),
                     subtotal * amount
