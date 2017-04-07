70e3a4e8aa72ae1b51d73dc8ea01dc130bb9618e
cardosan <giuseppe.cardellini@gmail.com>
2017-04-07 12:46:54 +0200

    update TD tests and get_maximum_value() for new np.datetime

--- bw2temporalis/tests/td.py
+++ bw2temporalis/tests/td.py
@@ -9,65 +9,86 @@ import unittest
 
 class TemporalDistributionTestCase(unittest.TestCase):
     def create_td(self):
-        return TD(np.arange(5), np.ones(5) * 2)
+        return TD(np.arange(0,5, dtype='timedelta64[Y]'), np.ones(5) * 2)
 
     def test_init(self):
+        """test TD inizialization"""
+        #empty
         with self.assertRaises(ValueError):
-            TD(None, None)
+            TD(None, None) 
+        #uneven lenght
         with self.assertRaises(ValueError):
             TD(np.arange(5), np.array(2, 2, 2, 2))
+        #float instead datetime
+        with self.assertRaises(ValueError):
+            TD(np.arange(5), np.ones(5) * 2)
 
     def test_mul_td(self):
+        """test __mul__ (i.e. convolution)"""
         td = self.create_td()
-        td2 = TD(np.array((-1., 0, 1.)), np.ones(3).astype(float))
+                
+        #test timedelta
+        td2 = TD(np.array((-1, 0, 1), dtype='timedelta64[Y]'), np.ones(3).astype(float))
+        #test datetime
+        td3 = TD(np.array((-1, 0, 1), dtype='datetime64[Y]'), np.ones(3).astype(float))
+        #convolution only possible with timedelta
+        with self.assertRaises(AssertionError):
+            td * td3
+        with self.assertRaises(AssertionError):
+            td3 * td
+        
         multiplied = td * td2
-        self.assertTrue(np.allclose(
-            np.arange(-1, 6),
-            multiplied.times
-        ))
+        #check result mul.times (allclose does not work with datetime)
+        self.assertTrue(np.array_equal(
+            np.arange(-1, 6, dtype='timedelta64[Y]').astype('timedelta64[s]'),
+            multiplied.times)
+        )
+        #check sum of values
         self.assertEqual(
             td.values.sum() * td2.values.sum(),
             multiplied.values.sum()
         )
+        #check result array mul.values
         self.assertTrue(np.allclose(
             np.array((2.,  4.,  6.,  6.,  6.,  4.,  2.)),
             multiplied.values
         ))
-
+        
     def test_div_td(self):
+        """check unpossible division between tds"""
         td = self.create_td()
-        td2 = TD(np.array((-1., 0, 1.)), np.ones(3).astype(float))
+        td2 = TD(np.array((-1, 0, 1), dtype='timedelta64[Y]'), np.ones(3).astype(float))
         with self.assertRaises(ValueError):
             td / td2
 
     def test_div_int(self):
+        """check possible division between td and int"""
         td = self.create_td()
         divided = td / 2.
-        self.assertTrue(np.allclose(
-            np.arange(5),
+        self.assertTrue(np.array_equal(
+            np.arange(0,5, dtype='timedelta64[Y]').astype('timedelta64[s]'),
             divided.times
         ))
-        self.assertTrue(np.allclose(
-            np.ones(5),
-            divided.values
-        ))
+
 
     def test_mul_int(self):
+        """check mul between td and int"""
         td = self.create_td() * 5
-        self.assertTrue(np.allclose(
+        self.assertTrue(np.array_equal(
             td.times,
-            np.arange(5)
+            np.arange(0,5, dtype='timedelta64[Y]').astype('timedelta64[s]'),
         ))
         self.assertTrue(np.allclose(
             td.values,
             np.ones(5) * 10
         ))
-
+        
     def test_add_integer(self):
+        """check add between td and int"""
         td = self.create_td() + 5
-        self.assertTrue(np.allclose(
+        self.assertTrue(np.array_equal(
             td.times,
-            np.arange(5)
+            np.arange(0,5, dtype='timedelta64[Y]').astype('timedelta64[s]'),
         ))
         self.assertTrue(np.allclose(
             td.values,
@@ -75,11 +96,12 @@ class TemporalDistributionTestCase(unittest.TestCase):
         ))
 
     def test_add_td(self):
+        """check add between tds"""
         td = self.create_td()
-        td2 = TD(np.array((-1., 0, 1.)), np.ones(3).astype(float))
+        td2 = TD(np.array((-1, 0, 1), dtype='timedelta64[Y]'), np.ones(3).astype(float))
         added = td + td2
-        self.assertTrue(np.allclose(
-            np.arange(-1, 5),
+        self.assertTrue(np.array_equal(
+            np.arange(-1, 5, dtype='timedelta64[Y]').astype('timedelta64[s]'),
             added.times
         ))
         self.assertEqual(
@@ -90,14 +112,15 @@ class TemporalDistributionTestCase(unittest.TestCase):
             np.array((1.,  3.,  3.,  2.,  2.,  2.)),
             added.values
         ))
+        
 
     def test_iter(self):
         td = iter(self.create_td())
-        self.assertEqual(next(td), (0, 2))
-        self.assertEqual(next(td), (1, 2))
-        self.assertEqual(next(td), (2, 2))
-        self.assertEqual(next(td), (3, 2))
-        self.assertEqual(next(td), (4, 2))
+        self.assertEqual(next(td), (np.timedelta64(0,'Y').astype('timedelta64[s]'), 2))
+        self.assertEqual(next(td), (np.timedelta64(1,'Y').astype('timedelta64[s]'), 2))
+        self.assertEqual(next(td), (np.timedelta64(2,'Y').astype('timedelta64[s]'), 2))
+        self.assertEqual(next(td), (np.timedelta64(3,'Y').astype('timedelta64[s]'), 2))
+        self.assertEqual(next(td), (np.timedelta64(4,'Y').astype('timedelta64[s]'), 2))
         with self.assertRaises(StopIteration):
             next(td)
 
--- bw2temporalis/utils.py
+++ bw2temporalis/utils.py
@@ -4,18 +4,20 @@ from eight import *
 
 from bw2data import Database
 from numbers import Number
-import arrow
 import numpy as np
 import warnings
 import re
+import datetime
 
 
+#also here need conversion from numpy to datetime
 def get_maximum_value(maybe_func, lower=None, upper=None, dynamic=True):
-    """Get maximum CF values by calculating each week for 100 years. Poor computers."""
+    """Get maximum CF values by calculating each week from now for 100 years. Poor computers."""
+    #GIU: approach below ~20 times faster than before
     if lower is None:
-        lower = arrow.get(2000, 1, 1)
+        lower = np.datetime64('now')
     if upper is None:
-        upper = arrow.get(2100, 1, 1)
+        upper = lower+np.timedelta64(100,'Y').astype('timedelta64[s]') 
     if isinstance(maybe_func, Number):
         return maybe_func
     def _(obj):
@@ -25,36 +27,38 @@ def get_maximum_value(maybe_func, lower=None, upper=None, dynamic=True):
             return sum([x.amount for x in obj])
     if dynamic:
         return max([_(maybe_func(x))
-            for x in arrow.Arrow.range('week', lower, upper)])
+            for x in np.arange(lower, upper, dtype='datetime64[W]').astype(datetime.timedelta)]) 
     else:
-        # If total CF not a function of time of emission,
-        # don't need to do all this work...
-        return _(maybe_func(lower))
+        #If total CF not a function of time of emission,
+        #don't need to do all this work, just calculate
+        #integral for all time horizon
+        # return _(maybe_func(lower))
+        return _(maybe_func(lower.astype(datetime.datetime)))
 
-
-def check_temporal_distribution_totals(name):
-    """Check that temporal distributions sum to total `amount` value"""
-    data = Database(name).load()
-    errors = []
-    for key, value in data.items():
-        if value.get('type', 'process') != 'process':
-            continue
-        for exchange in value.get('exchanges', []):
-            if 'temporal distribution' in exchange:
-                amount = exchange['amount']
-                total = sum([x[1] for x in exchange['temporal distribution']])
-                if not np.allclose(amount, total):
-                    errors.append({
-                        "output": key,
-                        "intput": exchange['input'],
-                        "expected": amount,
-                        "found": total
-                    })
-    if errors:
-        warnings.warn("Unbalanced exchanges found; see returned errors")
-        return errors
-    else:
-        return True
+#GIU:not needed anymore
+# def check_temporal_distribution_totals(name):
+    # """Check that temporal distributions sum to total `amount` value"""
+    # data = Database(name).load()
+    # errors = []
+    # for key, value in data.items():
+        # if value.get('type', 'process') != 'process':
+            # continue
+        # for exchange in value.get('exchanges', []):
+            # if 'temporal distribution' in exchange:
+                # amount = exchange['amount']
+                # total = sum([x[1] for x in exchange['temporal distribution']])
+                # if not np.allclose(amount, total):
+                    # errors.append({
+                        # "output": key,
+                        # "intput": exchange['input'],
+                        # "expected": amount,
+                        # "found": total
+                    # })
+    # if errors:
+        # warnings.warn("Unbalanced exchanges found; see returned errors")
+        # return errors
+    # else:
+        # return True
 
 
 function_re = re.compile("^def\s+(?P<func_name>\S+)\s*\(\s*\S*\s*(?:,\s*\S+)*\):", re.UNICODE)
