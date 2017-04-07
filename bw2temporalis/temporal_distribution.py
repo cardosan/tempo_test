84bc9cbd75618e26672018ccddde6779ea8471c7
cardosan <giuseppe.cardellini@gmail.com>
2017-04-07 08:43:09 +0200

    deep dyn_lca rework. No double traversal, uses numpy datetime in TD and added support for absolute date.

--- bw2temporalis/temporal_distribution.py
+++ bw2temporalis/temporal_distribution.py
@@ -5,31 +5,57 @@ from eight import *
 from bw2speedups import consolidate
 from future.utils import python_2_unicode_compatible
 import numpy as np
-
+import datetime
 
 @python_2_unicode_compatible
 class TemporalDistribution(object):
-    """An container for a series of values spread over time."""
+    """A container for a series of values spread over time.
+Args:
+    * *times* (ndarray): 1D array containg temporal info of `values` with type `timedelta64` or `datetime64` .
+    * *values* (ndarray): 1D array containg values with type `float` 
+    
+    Times and values must have same lenght and element of `values` must correspond to the element of `times`
+    with the same index.
+    """
     def __init__(self, times, values):
+        # #GIU: check if using non numpy datetime and timedelta does not slow down too much        
         try:
             assert isinstance(times, np.ndarray)
             assert isinstance(values, np.ndarray)
             assert times.shape == values.shape
+            #nasty but.... http://stackoverflow.com/a/23069878/4929813
+            assert 'timedelta64' in str(times.dtype) or \
+                   'datetime64' in str(times.dtype) or \
+                    isinstance(times[0], datetime.datetime),'times must be of type numpy datetime64 or timedelta64'
             # Type conversion needed for consolidate cython function
-            times = times.astype(np.float64)
             values = values.astype(np.float64)
         except AssertionError:
             raise ValueError(u"Invalid input values")
-        self.times = times
+            
+        #use always seconds as resolution (maybe not necessary?)
+        if 'datetime64' in str(times.dtype):
+            self.times = times.astype("datetime64[s]")
+        elif 'timedelta64' in str(times.dtype):
+            self.times = times.astype("timedelta64[s]")
+        else:
+            self.times=times #for datetime
         self.values = values
+        
+        
+    def __getitem__(self, val):
+        return TemporalDistribution(np.array(self.times[val]), np.array(self.values[val]))
 
     def __mul__(self, other):
-        if isinstance(other, TemporalDistribution):
+        if isinstance(other, TemporalDistribution):            
+            assert 'timedelta64' in str(self.times.dtype) and 'timedelta64' in str(other.times.dtype),"Multiplication between two TemporalDistribution possible only for timedelta"
             times = (self.times.reshape((-1, 1)) +
                      other.times.reshape((1, -1))).ravel()
             values = (self.values.reshape((-1, 1)) *
                       other.values.reshape((1, -1))).ravel()
-            return TemporalDistribution(*consolidate(times, values))
+            #use array view in consolidate, see http://stackoverflow.com/a/33528073/4929813
+            #need to reconvert times.view to timedelta64[s]
+            t_view,v=consolidate(times.view('int64'), values) 
+            return TemporalDistribution(t_view.astype('timedelta64[s]'),v)
         else:
             try:
                 return TemporalDistribution(self.times, self.values * float(other))
@@ -65,19 +91,22 @@ class TemporalDistribution(object):
 
     def __add__(self, other):
         if isinstance(other, TemporalDistribution):
+            assert 'timedelta64' in str(self.times.dtype),"sum between two TemporalDistribution possible only for timedelta"
             times = np.hstack((self.times, other.times))
             values = np.hstack((self.values, other.values))
-            return TemporalDistribution(*consolidate(times, values))
+            #same as in __mul__
+            t_view,v=consolidate(times.view('int64'), values) 
+            return TemporalDistribution(t_view.astype('timedelta64[s]'),v)
+
         else:
             try:
                 return TemporalDistribution(self.times, self.values + float(other))
             except:
                 raise ValueError(u"Can't add TemporalDistribution and %s" \
-                                 % type(other))
-
+                                 % type(other))        
     def __iter__(self):
         for index in range(self.times.shape[0]):
-            yield (float(self.times[index]), float(self.values[index]))
+            yield (self.times[index], float(self.values[index]))
 
     @property
     def total(self):
@@ -94,3 +123,19 @@ class TemporalDistribution(object):
     def cumulative(self):
         """Return new temporal distribution with cumulative values"""
         return TemporalDistribution(self.times, np.cumsum(self.values))
+        
+    def datetime_to_timedelta(self, dt):
+        """Convert TD.times of type datetime64 to timedelta64 based on the datetime64 passed
+        """
+        assert 'datetime64' in str(self.times.dtype),'TemporalDistribution.times must be numpy.datetime64'
+        assert isinstance(dt,np.datetime64),'datetime must be numpy.datetime64'
+        return  TemporalDistribution(self.times - dt, self.values)
+        
+    def timedelta_to_datetime(self, dt):
+        """Convert TD.times of type timedelta64 to datetime.datetime based on the datetime64 passed
+        """
+        #converted to datetime.datetime cause timeline._groupby_sum_by_flow is ~5 slower when using np.datetime64
+        #while the conversion here to datatime.datetime is almost the same performance wise 
+        assert 'timedelta64' in str(self.times.dtype),'TemporalDistribution.times must be numpy.datetime64'
+        assert isinstance(dt,np.datetime64),'datetime must be numpy.datetime64'
+        return  TemporalDistribution((self.times + dt).astype(datetime.datetime) , self.values)
